5f9aba4a1deae56d2887bb3d4d1b6e9e59b00baa
Jared <jaredtponting@gmail.com>
2024-08-20 20:39:49 +0100

    Project set up

---
+++ build/CMakeFiles/Pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pipeline.dir/main.cpp.obj"
+  "CMakeFiles/Pipeline.dir/main.cpp.obj.d"
+  "Pipeline.exe"
+  "Pipeline.exe.manifest"
+  "Pipeline.pdb"
+  "libPipeline.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,2 +1,6 @@
 #include <iostream>
 
+int main() {
+    std::cout << "High-Performance Data Processing Pipeline" << std::endl;
+    return 0;
+}
